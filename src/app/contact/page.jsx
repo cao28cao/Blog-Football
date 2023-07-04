0b5c15aed7fd8b157993f53e068824f900c21bae
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-07-04 07:42:02 +0700

    fix contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,15 +7,15 @@ const Contact = () => {
       <div className='px-16 py-8 text-[44px]'>
         <h1>Contact</h1>
         <h1>Let's Keep in Touch</h1>
-        <div className='flex'>
+        <div className='flex px-2 sm:px-4 py-16'>
           <div className='flex-1 w-3/5 p-8'>
             <Image src={keepInTouch} alt='keep in touch' />
           </div>
           <div className='flex-1 w-2/5'>
             <form className='flex flex-col space-y-4 text-[16px]'>
-              <input type="text" placeholder='Name' className='px-2 rounded-md'/>
-              <input type="text" placeholder='Email' className='px-2 rounded-md' />
-              <textarea cols="30" rows="10" placeholder='Messages' className='px-2 rounded-md'></textarea>
+              <input type="text" placeholder='Name' className='px-2 py-4 rounded-md text-black'/>
+              <input type="text" placeholder='Email' className='px-2 py-4 rounded-md text-black' />
+              <textarea cols="44" rows="10" placeholder='Messages' className='px-2 py-4 rounded-md text-black'></textarea>
             </form>
           </div>
         </div>
