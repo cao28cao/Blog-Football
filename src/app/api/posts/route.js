76d569bba06c4672d03064a7a857e6ba8aca14f9
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-07-11 16:09:07 +0700

    fix package

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+import connect from "@/utils/db"
+import Post from "@/app/models/Post"
+
+export const GET = async (req) => {
+    try {
+        await connect();
+        const posts = await Post.find();
+        return new NextResponse(posts, { status: 200 });
+        
+    } catch (error) {
+        return new NextResponse("Database connection failed", { status: 500 });
+    }
+
+};
