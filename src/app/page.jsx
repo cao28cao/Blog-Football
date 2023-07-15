12f5eff02d04c0b6db2fa99b63f97ac3132d8a72
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-07-15 09:46:22 +0700

    des home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='px-16 py-8 text-[44px]'>
-      Home
+      Home Page - but it's not des yet
     </div>
   )
 }
